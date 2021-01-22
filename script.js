1821b2057a4bbd3c4f1ac388bfeecf501df4963e
Vishal Kardam <vishalat1994@gmail.com>
2021-01-22 05:44:34 +0530

    Complete

--- script.js
+++ script.js
@@ -16,6 +16,7 @@ function renderColleges(list) {
     //render each card
     list.map(college => {
         const card = createNewElement('div', 'card');
+
         if(college["promoted"]){
             const promotion = createNewElement('div','promo')
             card.appendChild(promotion);
@@ -25,17 +26,105 @@ function renderColleges(list) {
             promotion.appendChild(topright);
             promotion.appendChild(bottomright);
         }
+
         const top = createNewElement('div', 'top-block');
         const image = createNewElement('img', 'banner');
         image.src = `./src/${college['image']}`; 
         const overlay = createNewElement('div', 'banner-overlay');
+
         const ratingBlock = createNewElement('div', 'rating-block');
+        const rating = createNewElement('div', 'rating');
+        const ratingRemark = createNewElement('div', 'rating-remark');
+        const outFive = createNewElement('div', 'outfive');
+
+        const topEnd = createNewElement('div', 'top-end');
+        const tags = createNewElement('div', 'tags');
+        college["tags"].map(tag => {
+            const pill = createNewElement('div', 'pill');
+            pill.innerHTML = tag;
+            tags.appendChild(pill);
+        });
+        const ranking = createNewElement('div', 'ranking');
+        const bottom = createNewElement('div', 'bottom-block');
+        const name = createNewElement('div', 'name');
+        name.innerHTML = college['college_name'];
+        const stars = createNewElement('div', 'stars');
+        for(let i=0; i < +college['rating']; i++){
+            stars.innerHTML += '&#9733;'; 
+        }
+        name.appendChild(stars);      
+
+        const rate = createNewElement('div', 'rate');
+        rate.innerHTML = (college['original_fees']).toLocaleString('en-IN', {
+            style: 'currency',
+            currency: 'INR',
+        }).slice(0, -3);
+        name.appendChild(rate);
+
+        const discount = createNewElement('div', 'discount');
+        
+        discount.innerHTML = college['discount'];
+        const triangle = createNewElement('div', 'triangle-left');
+        discount.appendChild(triangle);
+        name.appendChild(discount);
+
+        const nearest = createNewElement('div', 'nearest');
+        nearest.innerHTML = college['nearest_place'][0] + " | ";
+        const nearest_nxt = createNewElement('div', 'nearest-nxt');
+        nearest_nxt.innerHTML = college['nearest_place'][1];
+        nearest.appendChild(nearest_nxt);
+
+        const famousDiv = createNewElement('div', 'famous');
+        const match = createNewElement('div', 'match');
+        match.innerHTML = "93% Match : ";
+        famousDiv.appendChild(match);
+        famousDiv.innerHTML += college['famous_nearest_places'];
+
+        const offerDiv = createNewElement('div', 'offer');
+        const offerPill = createNewElement('div', 'offer-pill');
+        offerPill.innerHTML = college['offertext'];
+        offerDiv.appendChild(offerPill);
+        const amenties = createNewElement('div', 'amenties');
+        amenties.innerHTML = college['amenties'][0];
+        const dot = createNewElement('div', 'dot');
+        amenties.appendChild(dot);
+        amenties.innerHTML += college['amenties'][1];
+        offerDiv.appendChild(amenties);
+
+        const fee = createNewElement('div', 'fee');
+        fee.innerHTML = (college['discounted_fees']).toLocaleString('en-IN', {
+            style: 'currency',
+            currency: 'INR',
+        }).slice(0, -3);
+        const cycle = createNewElement('div', 'cycle');
+        cycle.innerHTML = college['fees_cycle'];
+        
+        famousDiv.appendChild(cycle);
+
+        nearest_nxt.appendChild(fee);
+
+        rating.innerHTML = college['rating'];
+        outFive.innerHTML = '/5'
+        ratingRemark.innerHTML = college['rating_remarks'];
+        ratingBlock.appendChild(rating);
+        ratingBlock.appendChild(outFive);
+        ratingBlock.appendChild(ratingRemark);
+
+        ranking.innerHTML = "#" + college['ranking'];
 
         card.appendChild(top);
         top.appendChild(image);
         top.appendChild(overlay);
-        
         top.appendChild(ratingBlock);
+        top.appendChild(topEnd);
+        topEnd.appendChild(tags);
+        topEnd.appendChild(ranking);
+
+        bottom.appendChild(name);
+        bottom.appendChild(nearest);
+        bottom.appendChild(famousDiv);
+        bottom.appendChild(offerDiv);
+        card.appendChild(bottom);
         document.getElementById("container").appendChild(card);
 
         count += 1;
